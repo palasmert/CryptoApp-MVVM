e76230387583364ccb27d842aa1b7a1a044e62cc
palasmert <134235672+palasmert@users.noreply.github.com>
2024-10-17 15:15:03 +0300

    urlrequest-endpoint

---
+++ CryptoAppMVVM/CryptoAppMVVM/API/HTTP.swift
@@ -0,0 +1,28 @@
+//
+//  HTTP.swift
+//  CryptoAppMVVM
+//
+//  Created by mert palas on 17.10.2024.
+//
+
+import Foundation
+
+enum HTTP {
+    
+    enum Method: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum Headers {
+        
+        enum Key: String {
+            case contentType = "ContentType"
+            case apiKey = "X-CMC_PRO_API_KEY"
+        }
+        
+        enum Value: String {
+            case applicationJson = "application/json"
+        }
+    }
+}
